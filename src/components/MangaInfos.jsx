80762be3dc8517c81c8b1f42bf13301c10352407
GMICHON <gauthier.michon@ynov.com>
2022-04-19 10:38:39 +0200

    page mangasinfo

--- src/components/MangaInfos.jsx
+++ src/components/MangaInfos.jsx
@@ -25,11 +25,29 @@ const MangaInfos = () => {
     return (
         <div className="mangas-list">
             {mangas.find(manga => manga.mal_id === parseInt(mangaId)).title}
-            <img src={mangas.find(manga => manga.mal_id === parseInt(mangaId)).images.jpg.image_url} alt="" />
+            <img src={
+                IfImageNotFound(mangas, mangaId)
+                } alt="image" />
+            <p>nombres de volumes publiés : {mangas.find(manga => manga.mal_id === parseInt(mangaId)).volumes}</p>
+            <p>nombres de chapitres publiés : {mangas.find(manga => manga.mal_id === parseInt(mangaId)).chapters}</p>
+            <p>{mangas.find(manga => manga.mal_id === parseInt(mangaId)).status}</p>
+            <p>Première publication le {mangas.find(manga => manga.mal_id === parseInt(mangaId)).published.string}</p>
+            <p>{mangas.find(manga => manga.mal_id === parseInt(mangaId)).synopsis}</p>
+            <p>Auteur : {mangas.find(manga => manga.mal_id === parseInt(mangaId)).authors[0].name}</p>
+
         </div>
     );
 
     
+}
+
+const IfImageNotFound = (mangas, mangaId) => {
+    let image = mangas.find(manga => manga.mal_id === parseInt(mangaId)).images.jpg.image_url
+    if (image == "https://cdn.myanimelist.net/images/manga/3/54525.jpg") {
+        return "https://cdn1.booknode.com/book_cover/498/full/monster-tome-1-herr-doktor-tenma-498116.jpg"
+    } else {
+        return image
+    }
 }
 
 export default MangaInfos;
